86424a63b16e127f55ab6e58330a8f0a72cef6cc
Shalane Proctor <shalane088@gmail.com>
2022-11-07 22:07:09 -0600

    Created PostDetails component & page, outline what is needed from promise call via proptypes, add merge data file to API folder

--- pages/Posts/[id].js
+++ pages/Posts/[id].js
@@ -1,39 +1,21 @@
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
-import { Badge, Button, Card } from 'react-bootstrap';
 import { getPostById } from '../../api/postData';
+import PostsDetails from '../../components/PostDetails';
 
 export default function PostsPage() {
-  const [postDetails, setPostDetails] = useState();
+  const [postDetail, setPostDetail] = useState();
   const router = useRouter();
 
   const { id } = router.query;
 
   useEffect(() => {
-    getPostById(id).then(setPostDetails);
+    getPostById(id).then(setPostDetail);
   }, [id]);
 
   return (
-    <Card className="text-center">
-      <Card.Title>{postDetails.title}</Card.Title>
-      <Card.Img variant="top" src={postDetails.image_url} />
-      <Card.Body>
-        <Card.Text>Author: soandso</Card.Text>
-        <Button>View Comments</Button>
-        <Badge pill bg="primary">
-          😍
-        </Badge>
-        <Badge pill bg="primary">
-          🤯
-        </Badge>
-        <Badge pill bg="primary">
-          🤔
-        </Badge>
-        <Badge pill bg="primary">
-          🤬
-        </Badge>
-        <Card.Text>{postDetails.content}</Card.Text>
-      </Card.Body>
-    </Card>
+    <div className="center-page">
+      <PostsDetails key={postDetail?.id} postObj={postDetail} />
+    </div>
   );
 }
